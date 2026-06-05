b41940853f123c7f850f4a321c1bd0a647492f60
Tchangop-Gaby <gabytchangop@gmail.com>
2026-06-05 19:30:43 +0100

    Update init_database.sql

--- scripts/db/init_database.sql
+++ scripts/db/init_database.sql
@@ -1,1 +1,19 @@
+/*
+===============================================================================
+Create Bronze, Silver, and Gold Schemas
+===============================================================================
+Description:
+    Create the Schema Structure for the Medallion Architecture
+    used in the Data Warehouse.
 
+Precautions:
+    Follow the guide in the Readme on how to create the database
+    and run this file.
+===============================================================================
+*/
+
+\connect datawarehouse
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
